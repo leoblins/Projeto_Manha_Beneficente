cf5ee058ada5ef8d4a1900fe7d574934fe4a857a
Leonardo Lins Barbosa <leoblins@gmail.com>
2025-06-06 11:54:42 -0300

    Adiciona comando para criação automática de superusuário

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
 python manage.py create_admin
-
+gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
