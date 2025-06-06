0cde3c31645240761d71fa857ef6d150bfef8e50
Leonardo Lins Barbosa <leoblins@gmail.com>
2025-06-06 12:17:11 -0300

    Garante superusuário leoblins no deploy

--- doacoes/management/commands/create_admin.py
+++ doacoes/management/commands/create_admin.py
@@ -2,16 +2,23 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Cria um superusuário automaticamente'
+    help = 'Remove qualquer superusuário e cria um novo com credenciais fixas'
 
     def handle(self, *args, **options):
         User = get_user_model()
-        username = 'admin'
+        username = 'leoblins'
+        email = 'leoblins@gmail.com'
         password = 'Sardinha221405@'
-        email = 'admin@example.com'
 
+        # Apaga todos os superusuários existentes
+        superusers = User.objects.filter(is_superuser=True)
+        for user in superusers:
+            self.stdout.write(self.style.WARNING(f'Removendo superusuário: {user.username}'))
+            user.delete()
+
+        # Cria novo superusuário
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS('Superusuário criado com sucesso'))
+            self.stdout.write(self.style.SUCCESS(f'Superusuário {username} criado com sucesso'))
         else:
-            self.stdout.write(self.style.WARNING('Superusuário já existe'))
+            self.stdout.write(self.style.WARNING(f'Usuário {username} já existe, mas não é superusuário'))
--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py create_admin
-python manage.py check_admin
 gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
